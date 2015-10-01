21db64094f9887533d25efc077fad9e43cc955dc
wircho <correo.de.adolfo@gmail.com>
2015-10-01 10:58:10 -0400

    Made timeout the default 60 seconds

--- PBWebViewController/PBWebViewController.m
+++ PBWebViewController/PBWebViewController.m
@@ -50,7 +50,7 @@
 
 - (void)load
 {
-    NSURLRequest *request = [NSURLRequest requestWithURL:self.URL cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:4.0];
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.URL cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:60.0];
     [self.webView loadRequest:request];
     
     if (self.navigationController.toolbarHidden) {
